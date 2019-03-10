f95cbbd004c7c6f01e78e98529228ccd5c8832cd
zhengzhanpeng <714586750@qq.com>
2019-03-10 09:07:22 +0800

    [zhanpeng] init spring boot projectˆ

---
+++ src/main/java/com/otr/gocd/workshorp/GocdApplication.java
@@ -0,0 +1,13 @@
+package com.otr.gocd.workshorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GocdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GocdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otr/gocd/workshorp/GocdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otr.gocd.workshorp;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GocdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
